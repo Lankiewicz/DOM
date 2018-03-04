6bfd81e62bd316631b3fde1a699b337ccf9f69d8
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-03-04 17:14:52 +0100

    add js code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var withButtom = document.getElementsByClassName('buttom');
+var buttom = 'Hello!';
+console.log(buttom.length);
+for (var i = 0; i < 6; i++) {
+ document.getElementsByClassName('buttom').innerText
+}
+console.log(window);

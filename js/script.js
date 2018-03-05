a064880ca7755037cc1802f02d0ce3dc87781a5b
Lankiewicz <36169986+Lankiewicz@users.noreply.github.com>
2018-03-05 14:22:41 +0100

    correction2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var withButtom = document.getElementsByClassName('buttom');
 var buttom = 'Hello!';
-console.log(buttom.length);
 for (var i = 0; i < withButtom.lenght; i++) {
 var text = document.getElementsByClassName('buttom').innerText
 console.log(text)
